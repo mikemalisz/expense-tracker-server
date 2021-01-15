df0dcc66b227b99e939ec0bfa90c43f721a0143a
Mike Maliszewski <mikemalisz@gmail.com>
2021-01-15 14:50:52 -0600

    added dotenv package, created config file for environment variable access, app now uses config for constants, added .env to .gitignore

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express'
+import config from './config'
 
 const app = express()
-const PORT = 3000
 
 app.get('/', (request, response) => {
    response.send("hello world!")
 })
 
-app.listen(PORT, () => {
-   console.log(`Server started on localhost:${PORT}`)
+app.listen(config.PORT, () => {
+   console.log(`Server started on localhost:${config.PORT}`)
 })
